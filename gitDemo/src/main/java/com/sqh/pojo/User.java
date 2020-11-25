6291c8baca6a59e5c66a16c05ec9f96f635462d2
huazaisqh <7833077+huazaisqh@user.noreply.gitee.com>
2020-11-26 02:05:56 +0800

    update gitDemo/src/main/java/com/sqh/pojo/User.java. 最新更新

--- gitDemo/src/main/java/com/sqh/pojo/User.java
+++ gitDemo/src/main/java/com/sqh/pojo/User.java
@@ -32,5 +32,6 @@ public class User {
         System.out.println("创建分支");
         System.out.println("主干添加");
         System.out.println("分支添加");
-    }
+        System.out.println("最新添加"); 
+     }
 }
