aa387c3de58944ef3d3527280d90c38f7edee2ad
admin <zhangsan@itcast.cn>
2020-11-26 01:31:13 +0800

    第一次修改

---
+++ gitDemo/src/main/java/com/sqh/App.java
@@ -0,0 +1,13 @@
+package com.sqh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
