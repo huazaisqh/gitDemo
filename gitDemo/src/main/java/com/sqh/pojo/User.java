c9ffebad8365f564685bb6cf46f4d69a647239ce
huazaisqh <shuqinghua@gcsoft.com>
2020-11-26 20:31:48 +0800

    zhangsan

--- gitDemo/src/main/java/com/sqh/pojo/User.java
+++ gitDemo/src/main/java/com/sqh/pojo/User.java
@@ -32,6 +32,7 @@ public class User {
         System.out.println("创建分支");
         System.out.println("主干添加");
         System.out.println("分支添加");
-        System.out.println("最新添加"); 
+        System.out.println("最新添加");
+        System.out.println("zhangsan");
      }
 }
